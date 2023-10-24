adc7e19984e88d6686cf3cb4005243b501fa3b1c
antoinel74 <antoine.lansman@gmail.com>
2023-10-24 15:23:32 +0200

    replace img with webp + modif style

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".navbar");
 const navHome = document.querySelector(".home");
+const navContact = document.querySelector(".contacts");
 const navServices = document.querySelector(".services");
 const navCTA = document.getElementById("navbar-cta");
 
@@ -9,19 +10,19 @@ let navToggle = () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     navHome.classList.toggle("active");
+    navContact.classList.toggle("active");
     navCTA.classList.toggle("active");
     navServices.classList.toggle("active");
   });
 
-  // Get all navigation links
   const navLinks = document.querySelectorAll(".nav-link");
 
-  // Add click event listeners to each navigation link
   navLinks.forEach((n) => {
     n.addEventListener("click", () => {
       hamburger.classList.remove("active");
       navMenu.classList.remove("active");
       navHome.classList.remove("active");
+      navContact.classList.remove("active");
       navCTA.classList.remove("active");
       navServices.classList.remove("active");
     });
